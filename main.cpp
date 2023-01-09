c0be1aa9679cea880d3cb1e731796e5ee952824d
zwmain <409435808@qq.com>
2023-01-09 14:40:40 +0800

    Change test method

--- main.cpp
+++ main.cpp
@@ -1,13 +1,23 @@
 #include "ThreadPool.hpp"
+#include <chrono>
 #include <iostream>
+#include <mutex>
+#include <thread>
+
+std::mutex mtxCout;
 
 int test(int a)
 {
+    std::chrono::milliseconds waitTime(500 - a * 10);
+    std::this_thread::sleep_for(waitTime);
+    std::unique_lock<std::mutex> lock(mtxCout);
+    std::cout << "当前值为：" << a << std::endl;
     return 2 * a;
 }
 
 int main()
 {
+    auto startTime = std::chrono::system_clock::now();
     size_t sys_thread_size = std::thread::hardware_concurrency();
     zwn::ThreadPool pool(sys_thread_size);
     std::cout << "线程池大小：" << pool.size() << std::endl;
@@ -21,8 +31,17 @@ int main()
     }
 
     for (auto& i : resVec) {
-        std::cout << "结果：" << i.get() << std::endl;
+        i.wait();
     }
 
+    std::cout << "输出结果：" << std::endl;
+    for (auto& i : resVec) {
+        std::cout << i.get() << std::endl;
+    }
+
+    auto endTime = std::chrono::system_clock::now();
+    auto totalTime = startTime - endTime;
+    long long int val = std::chrono::duration_cast<std::chrono::milliseconds>(totalTime).count();
+    std::cout << "总用时：" << val << "ms" << std::endl;
     return 0;
 }
